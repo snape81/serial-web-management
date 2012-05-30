c0329ff47e9ac79293b90b94f189b0c9be364c42
snape81 <claudio.starnoni@gmail.com>
2012-05-30 19:21:52 +0200

    first import ( disaster recovery ) not working

--- server/src/main/java/com/nexse/serial/server/rxtx/RxTxCommunicationReader.java
+++ server/src/main/java/com/nexse/serial/server/rxtx/RxTxCommunicationReader.java
@@ -14,56 +14,100 @@ public class RxTxCommunicationReader {
         super();
     }
 
-    void connect(String portName) throws Exception {
-
+    void connect ( String portName ) throws Exception
+    {
         CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
-
-        if (portIdentifier.isCurrentlyOwned()) {
+        if ( portIdentifier.isCurrentlyOwned() )
+        {
             System.out.println("Error: Port is currently in use");
-        } else {
-            CommPort commPort = portIdentifier.open(this.getClass().getName(), 2000);
+        }
+        else
+        {
+            CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
 
-            if (commPort instanceof SerialPort) {
+            if ( commPort instanceof SerialPort )
+            {
                 SerialPort serialPort = (SerialPort) commPort;
-                serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+                serialPort.setSerialPortParams(9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
 
                 InputStream in = serialPort.getInputStream();
                 OutputStream out = serialPort.getOutputStream();
 
                 (new Thread(new SerialReader(in))).start();
+                (new Thread(new SerialWriter(out))).start();
 
-            } else {
+            }
+            else
+            {
                 System.out.println("Error: Only serial ports are handled by this example.");
             }
         }
     }
 
     /** */
-    public static class SerialReader implements Runnable {
+    public static class SerialReader implements Runnable
+    {
         InputStream in;
 
-        public SerialReader(InputStream in) {
+        public SerialReader ( InputStream in )
+        {
             this.in = in;
         }
-        public void run() {
+
+        public void run ()
+        {
             byte[] buffer = new byte[1024];
             int len = -1;
-            try {
-                while ((len = this.in.read(buffer)) > -1) {
-                    System.out.print(new String(buffer, 0, len));
+            try
+            {
+                while ( ( len = this.in.read(buffer)) > -1 )
+                {
+                    System.out.print(new String(buffer,0,len));
                 }
-            } catch (IOException e) {
+            }
+            catch ( IOException e )
+            {
                 e.printStackTrace();
             }
         }
     }
 
+    /** */
+    public static class SerialWriter implements Runnable
+    {
+        OutputStream out;
 
+        public SerialWriter ( OutputStream out )
+        {
+            this.out = out;
+        }
 
-    public static void main(String[] args) {
-        try {
+        public void run ()
+        {
+            try
+            {
+                int c = 0;
+                while ( ( c = System.in.read()) > -1 )
+                {
+                    this.out.write(c);
+                }
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main ( String[] args )
+    {
+        try
+        {
             (new RxTxCommunicationReader()).connect("/dev/ttyUSB0");
-        } catch (Exception e) {
+        }
+        catch ( Exception e )
+        {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
